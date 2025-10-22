86784d6748a05ec2cfa9b23bb8587fb4746ec89f
ngezLin <89445359+ngezLin@users.noreply.github.com>
2025-10-22 16:04:49 +0700

    fix: lanyard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Page() {
       <Pricing />
       <Gallery />
       {/* <Lanyard /> */}
-      <Merchandise />
+      {/* <Merchandise /> */}
       <AboutMap />
       {/* <Footer /> */}
       {/* <WhatsAppButton /> */}

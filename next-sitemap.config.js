9397ea451924d43cc4a6be0de022bcd0990a2af7
ngezLin <89445359+ngezLin@users.noreply.github.com>
2025-10-30 13:04:28 +0700

    feat: add next-sitemap configuration and update robots.txt and sitemap.xml for improved SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://kolamrenangkingkong.com", // ganti sesuai domainmu
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};

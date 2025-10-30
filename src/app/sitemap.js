3a2e4ff2e9d193bd0b5daff870732022d9df3013
ngezLin <89445359+ngezLin@users.noreply.github.com>
2025-10-30 15:20:17 +0700

    feat: remove unused URLs from sitemap.js for improved SEO

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -8,23 +8,5 @@ export default function sitemap() {
       changeFrequency: "weekly",
       priority: 1.0,
     },
-    {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/tiket`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/kontak`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }

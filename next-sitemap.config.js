9bddb8d6c1fa5085b8770e815721196e9fc0c362
ngezLin <89445359+ngezLin@users.noreply.github.com>
2025-11-03 00:29:34 +0700

    feat: remove next-sitemap dependency from package.json and package-lock.json

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://kolamrenangkingkong.com",
-  generateRobotsTxt: true,
-  changefreq: "weekly",
-  priority: 0.8,
-  sitemapSize: 5000,
-};

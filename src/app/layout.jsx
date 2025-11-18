65c56bc45c165180993a14adc29ea76d6d575e55
ngezlin <vincentiusf17@gmail.com>
2025-11-18 10:01:58 +0700

    fix: duplicate icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -52,14 +52,12 @@ export const metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     shortcut: "/favicon.ico",
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
     other: [
       {
         rel: "icon",
